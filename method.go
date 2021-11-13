7ca1d0718b0894b54d20f9114dd4af6e2704110d
ho-ryu-ji <ryuta.soc7@gmail.com>
2021-11-13 16:43:39 +0900

    added method.go

---
+++ method.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
+
+func main() {
+	r1 := Rectangle{12, 2}
+	r2 := Rectangle{9, 4}
+	c1 := Circle{10}
+	c2 := Circle{25}
+
+	fmt.Println("S.r1 = ", r1.area())
+	fmt.Println("S.r2 = ", r2.area())
+	fmt.Println("S.c1 = ", c1.area())
+	fmt.Println("S.c1 = ", c2.area())
+}
